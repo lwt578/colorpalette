835675dda7427158a67adfc09b39a7fe7eba3420
lwt578 <lwt578@126.com>
2023-01-02 14:09:34 +0800

    1

--- colors.R
+++ colors.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 
+# 色板 ----
 
 clr <- data.frame(no=1:657,color=colors()) %>% 
   rbind(data.frame(no=658:660,color=rep('white',3)))
@@ -16,6 +17,35 @@ ggplot(clr, aes(xmin=x-12,xmax=x+12, ymin=y-1,ymax=y+1)) +
   theme_void()+
   geom_text(aes(x,y-2,label=color),size=4)
 
+# 色板升级版 ----
+
+clr <- data.frame(no=1:657,color=colors()) %>% 
+  rbind(data.frame(no=658:660,color=rep('white',3)))
+
+clr <- clr %>% 
+  mutate(l=rep(1:10,each=66)) %>% 
+  mutate(x=rep(1:6*5,110)) %>% 
+  mutate(y=rep(rep(1:11*3,each=6),10)) %>% 
+  slice_head(n=657)
+
+l <- split(clr,clr$l)
+
+ggplot(l[[1]], aes(xmin=x-2,xmax=x+2, ymin=y-1,ymax=y+1)) +
+  geom_rect(aes(fill=I(color)),colour = "black")+
+  lims(y=c(35,0))+
+  theme_void()+
+  geom_text(aes(x,y-1.5,label=color),size=3)
 
 
+f <- function(df){
+  ggplot(df, aes(xmin=x-2,xmax=x+2, ymin=y-1,ymax=y+1)) +
+    geom_rect(aes(fill=I(color)),colour = "black")+
+    lims(y=c(35,0))+
+    theme_void()+
+    geom_text(aes(x,y-1.5,label=color),size=3)
+  
+  
+}
 
+photo <- map(l,f)
+photo
