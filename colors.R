0a1f14d27bccc851212a8acc23f98e313d552638
lwt578 <lwt578@126.com>
2023-01-10 22:15:43 +0800

    3

--- colors.R
+++ colors.R
@@ -1,7 +1,7 @@
-library(tidyverse)
-library(grid)
 
 # 色板(grid) ----
+
+library(grid)
 
 a <- colors()
 
@@ -14,19 +14,18 @@ grid.rect(x = rep(seq(0.04,0.96,length=22),times=30),
 grid.text(a,
           x=rep(seq(0.04,0.96,length=22),times=30),
           y = rep(seq(0.965,0.065,length=30),each=22),
-          gp=gpar(fontsize=15)
-)
+          gp=gpar(fontsize=15))
+
 
 # 色板(ggplot2) ----
 
-clr <- data.frame(no=1:657,color=colors()) %>% 
-  rbind(data.frame(no=658:660,color=rep('white',3)))
+library(tidyverse)
 
-clr <- clr %>% 
+clr <- data.frame(no=1:657,color=colors()) %>% 
+  rbind(data.frame(no=658:660,color=rep('white',3)))%>% 
   mutate(x=rep(1:33*30,20)) %>% 
   mutate(y=rep(1:20*5,each=33)) %>% 
   slice_head(n=657)
-
 
 ggplot(clr, aes(xmin=x-12,xmax=x+12, ymin=y-1,ymax=y+1)) +
   geom_rect(aes(fill=I(color)),colour = "black")+
@@ -34,7 +33,13 @@ ggplot(clr, aes(xmin=x-12,xmax=x+12, ymin=y-1,ymax=y+1)) +
   theme_void()+
   geom_text(aes(x,y-2,label=color),size=4)
 
+ggsave('colors.pdf',limitsize = FALSE,
+       width = 50,height = 30,device = 'pdf')
+
+
 # 色板升级版(去掉重复颜色) ----
+
+library(tidyverse)
 
 clr <- data.frame(no=1:502,color=colors(TRUE)) %>% 
   rbind(data.frame(no=503:510,color=rep('white',8))) %>% 
@@ -46,7 +51,6 @@ clr <- data.frame(no=1:502,color=colors(TRUE)) %>%
 
 l <- split(clr,clr$l)
 
-
 f <- function(df){
   ggplot(df, aes(xmin=x-2,xmax=x+2, ymin=y-1,ymax=y+1)) +
     geom_rect(aes(fill=I(color)),colour = "black")+
@@ -55,7 +59,11 @@ f <- function(df){
     geom_text(aes(x,y-2,label=color),size=4)
 }
 
+for(i in 1:length(l)){
+  f(l[[i]])
+  ggsave(paste0(i,'.pdf'),
+         width = 8.27,height = 11.69,device = 'pdf')
+}
 
-photo <- map(l,f)
-photo
-  
+
+
